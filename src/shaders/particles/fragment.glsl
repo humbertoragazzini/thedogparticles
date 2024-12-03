7727c9143e82ec89e1920defacfc1a379825c545
beto <beto_vicio@hotmail.com>
2024-12-03 22:07:47 +0000

    remove distance from the frag color starting with uniforms

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -5,9 +5,12 @@ void main()
     // float distanceToCenter = distance(uv, vec2(0.5));
     float distanceToCenter = length(uv - vec2(0.5));
 
+    // discard make the shader not render the pixel in the position
+    if(distanceToCenter > 0.5){
+        discard;
+    }
 
-
-    gl_FragColor = vec4(vec3(distanceToCenter), 1.0);
+    gl_FragColor = vec4(1.0,1.0,1.0, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
