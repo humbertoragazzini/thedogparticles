c1a3d08c500b79855ebd874f22619668952230e9
beto <beto_vicio@hotmail.com>
2024-12-03 22:02:36 +0000

    added uv distance to particle

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -1,6 +1,13 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+    vec2 uv = gl_PointCoord;
+    // float distanceToCenter = distance(uv, vec2(0.5));
+    float distanceToCenter = length(uv - vec2(0.5));
+
+
+
+    gl_FragColor = vec4(vec3(distanceToCenter), 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
