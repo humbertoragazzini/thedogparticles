0b1efc20031dea510ae08653c9e6ad65bc5380d9
beto <beto_vicio@hotmail.com>
2024-12-03 22:22:22 +0000

    added texture and some varyings and uniforms to start picture effect

--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -1,14 +1,23 @@
 uniform vec2 uResolution;
+uniform sampler2D uPictureTexture;
+
+varying vec3 vColor;
 
 void main()
 {
-    // Final position
+    // Final position 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
+    // picture
+    float pictureIntensity = texture(uPictureTexture, uv).r;
+
     // Point size
-    gl_PointSize = 0.3 * uResolution.y;
+    gl_PointSize = pictureIntensity * 0.3 * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+
+    // varyings
+    vColor = vec3(pictureIntensity);
 }
